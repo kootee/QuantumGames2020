0c657f447611b98fe0a68c136ef5145ae7a4f018
kootee <katja.toivola@aalto.fi>
2020-04-03 11:50:33 +0300

    added character

--- main.py
+++ main.py
@@ -2,26 +2,23 @@ import pew
 
 
 pew.init()
-screen = pew.Pix.from_iter((
-    (1, 1, 1, 1, 1, 1, 1, 1),
-    (1, 0, 0, 0, 0, 0, 0, 1),
-    (1, 1, 3, 1, 0, 0, 0, 1),
-    (1, 0, 0, 1, 0, 1, 1, 1),
-    (1, 0, 0, 3, 0, 2, 0, 1),
-    (1, 0, 2, 1, 0, 1, 0, 1),
-    (1, 0, 0, 0, 0, 1, 0, 1),
-    (1, 1, 1, 1, 1, 1, 1, 1),
-))
+screen = pew.Pix()
 
-x = 4
-y = 1
-blink = True
+for i in range(8):
+    for j in range(8):
+        screen.pixel(i,j,0)
+
+screen.pixel(5,5,3)
+x = 0
+y = 0
 
 while True:
-    screen.pixel(x, y, 0 if screen.pixel(x, y) < 4 else 2)
+    screen.pixel(x,y,0)
     keys = pew.keys()
     dx = 0
     dy = 0
+
+    #arrow key controls
     if keys & pew.K_UP:
         dy = -1
     elif keys & pew.K_DOWN:
@@ -30,23 +27,15 @@ while True:
         dx = -1
     elif keys & pew.K_RIGHT:
         dx = 1
-    target = screen.pixel(x + dx, y + dy)
-    behind = screen.pixel(x + dx + dx, y + dy + dy)
-    if target in {0, 2}:
+
+    target = screen.pixel(x + dx ,y + dy)
+    #constrains the movement of the character
+    if target == 0: #move to empty spot
         x += dx
         y += dy
-    elif target in {3, 7} and behind in {0, 2}:
-        screen.pixel(x + dx + dx, y + dy + dy, 3 if behind == 0 else 7)
-        x += dx
-        y += dy
-    count = 0
-    for b in range(8):
-        for a in range(8):
-            if screen.pixel(a, b) == 2:
-                count += 1
-    if count == 0:
-        break
-    screen.pixel(x, y, (3 if blink else 2) + (4 if screen.pixel(x, y) in {2, 7} else 0))
-    blink = not blink
+    elif target == 3:
+        print('door')
+        #a door to a different floor
+    screen.pixel(x,y,3)
     pew.show(screen)
     pew.tick(1/6)
---
+++ main1.py
@@ -0,0 +1,60 @@
+import pew
+
+
+pew.init()
+
+
+screen = pew.Pix.from_iter((
+    (1, 1, 1, 1, 1, 1, 1, 1),
+    (1, 0, 0, 0, 0, 0, 0, 1),
+    (1, 1, 3, 1, 0, 0, 0, 1),
+    (1, 0, 0, 1, 0, 1, 1, 1),
+    (1, 0, 0, 3, 0, 2, 0, 1),
+    (1, 0, 2, 1, 0, 1, 0, 1),
+    (1, 0, 0, 0, 0, 1, 0, 1),
+    (1, 1, 1, 1, 1, 1, 1, 1),
+))
+
+x = 4
+y = 1
+blink = True
+
+#game loop
+while True:
+    screen.pixel(x, y, 0 if screen.pixel(x, y) < 4 else 2) #if the pixel is <4 (not yellow), then set the pixel to red
+    #print(screen.pixel(x,y))
+    keys = pew.keys()
+    dx = 0
+    dy = 0
+    if keys & pew.K_UP:
+        dy = -1
+    elif keys & pew.K_DOWN:
+        dy = 1
+    elif keys & pew.K_LEFT:
+        dx = -1
+    elif keys & pew.K_RIGHT:
+        dx = 1
+
+    target = screen.pixel(x + dx, y + dy)
+    behind = screen.pixel(x + dx + dx, y + dy + dy)
+    print('target ', target)
+    #print('behind ',behind)
+    if target in {0, 2}:
+        x += dx
+        y += dy
+    elif target in {3, 7} and behind in {0, 2}:
+        screen.pixel(x + dx + dx, y + dy + dy, 3 if behind == 0 else 7)
+        x += dx
+        y += dy
+    count = 0
+    #print(count)
+    for b in range(8):
+        for a in range(8):
+            if screen.pixel(a, b) == 2:
+                count += 1
+    if count == 0:
+        break
+    screen.pixel(x, y, (3 if blink else 2) + (4 if screen.pixel(x, y) in {2, 7} else 0))
+    blink = not blink
+    pew.show(screen)
+    pew.tick(1/6)
