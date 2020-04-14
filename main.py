574a010654aa994e9ac94b8fff505a4ed00a3890
kootee <katja.toivola@aalto.fi>
2020-04-14 21:03:09 +0300

    map functions modified

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 import pew
 import microqiskit
+from map import level
 
 pew.init()
 screen = pew.Pix()
 ammo = 8
+world_x = 1
+world_y = 1
+grid_location = 5
+grid = level
 
-screen = pew.Pix.from_iter((
-    (1, 1, 1, 1, 1, 1, 1, 1),
-    (0, 1, 0, 0, 0, 0, 0, 1),
-    (1, 1, 1, 0, 0, 0, 0, 1),
-    (1, 1, 1, 0, 0, 1, 0, 1),
-    (1, 1, 1, 0, 0, 1, 0, 1),
-    (1, 0, 0, 0, 0, 0, 1, 1),
-    (1, 1, 1, 0, 0, 0, 1, 1),
-    (3, 3, 3, 3, 3, 3, 3, 3),
-))
+#displays screen based on "world coordinates"
+def update_screen(grid_location):
+    #cells = [i for i in range((grid_location * 8 - 8),(grid_location * 8))]
+    current_screen = [grid[i] for i in range((grid_location * 8 - 8),(grid_location * 8))]
+    print(current_screen)
+    return current_screen
 
-#initialise screen with only black (0) pixels
-""" for i in range(8):
-    for j in range(8):
-        if j < 7:
-            screen.pixel(i,j,0)
-        else:
-            screen.pixel(i,j,3)
- """
-screen.pixel(5,4,2) #test location of ghost
+current_screen = update_screen(grid_location)
+screen = pew.Pix.from_iter(current_screen)
+
 status = True
 #initial state of player
 x = 1
 y = 1
 
-'''End game (not working yet), shows the text GAME OVER! and the stats. Game ends when you press X'''
+'''End game (not working yet), 
+shows the text GAME OVER! and the stats. 
+Game ends when you press X'''
 def end_game():
     print('you are out of ammo')
     text = pew.Pix.from_text('Game over!') #text will be shown when game ends also shwoing stat
@@ -46,37 +43,51 @@ def catch(s):
     return s
     #this should trigger the catching process ie creating the entangled photons
 
-def update_pixels(dx, dy): #loop through the pixels and update them 
-    print('update')
-    print(screen.pixel(x,y))
+def update_pixels(dx, dy): #loop through the pixels and update them to show map
+    
     if y > 5 and dy == 1:
         print('y bigger than five') 
         for i in range(8):
             for j in range(8):
-                pic_before = screen.pixel(i, j + 1)
+                if i == 7:
+                    pic_before = grid[i + world_y][j]
+                    print(pic_before)
+                    print(grid[i + world_y][j])
+                else:
+                    pic_before = screen.pixel(i, j + 1)
+                
                 screen.pixel(i,j,pic_before)
+    
     elif y < 2 and dy == -1:
         print('y smaller than two')
         for i in range(8):
             for j in range(8):
                 pic_before = screen.pixel(i, j - 1)
                 screen.pixel(i,j,pic_before)
+    
     elif x > 5 and dx == 1:
         print('x bigger than five')
         for i in range(8):
             for j in range(8):
                 pic_before = screen.pixel(i + 1, j)
                 screen.pixel(i,j,pic_before)
+    
     elif x < 2 and dx == -1:
         print('x smaller than two')
-        for i in range(1, 9):
+        for i in range(8):
             for j in range(8):
                 x_prev_pix = i - 1
                 y_prev_pix = j
-                pic_before = screen.pixel(x_prev_pix, y_prev_pix)
+                if x_prev_pix < 0:
+                    print('going offscreen')
+                    pic_before = screen.pixel(x_prev_pix, y_prev_pix)
+                    #look at coord
+                else:
+                    pic_before = screen.pixel(x_prev_pix, y_prev_pix)
                 print('i:', i, 'j:', j)
                 print('color', pic_before)
                 #screen.pixel(i,j,pic_before)
+    
     print('no update')
     # - dx or dy
  
@@ -130,6 +141,9 @@ while status:
             print('x and y:', x , y)
             print('dx and dy', dx , dy)
             update_pixels(dx,dy)
+        world_x += dx
+        world_y += dy
+        print('world coordinates', world_x, world_y)
     elif target == 3:
         print('door')#a door to a different floor TODO move to a different floor
     elif target == 2:
